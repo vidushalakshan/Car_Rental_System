7eec48bae92222f96b02caae0259f76c1b05ae3e
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2022-07-13 17:13:22 +0530

    aded packages and resources

---
+++ src/test/java/spring/service/impl/ItemServiceImplTest.java
@@ -0,0 +1,35 @@
+package spring.service.impl;
+
+import lk.ijse.spring.config.WebAppConfig;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.List;
+
+/**
+ * @author Helitha Sri
+ * @created 6/30/2022 - 11:36 AM
+ * @project Spring POS
+ */
+
+@WebAppConfiguration
+@ContextConfiguration(classes = {WebAppConfig.class})
+@ExtendWith(SpringExtension.class)
+class ItemServiceImplTest {
+
+    @Autowired
+    PurchaseOrderService service;
+
+    @Autowired
+    ItemService itemService;
+
+    @Test
+    void getAllItemIds() {
+        List<String> allItemIds = itemService.getAllItemIds();
+        System.out.println("a : "+allItemIds);
+    }
+}
