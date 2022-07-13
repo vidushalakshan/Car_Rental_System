a684c0b02a59c974101cad70e5359413d4633e69
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2022-07-13 20:28:27 +0530

    updated vehical entity

--- src/main/java/lk/ijse/spring/entity/vehical.java
+++ src/main/java/lk/ijse/spring/entity/vehical.java
@@ -1,5 +1,25 @@
 package lk.ijse.spring.entity;
 
-public class vehical {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class vehical {
+    @Id
+    private String re_Num;
+    private String vehical_Type_ID;
+    private String fuel_Type;
+    private String color;
+    private String Transmission_Type;
+    private double running_Km;
+    private int no_Of_Passenger;
+    private String brand;
+    private String status;
 }
