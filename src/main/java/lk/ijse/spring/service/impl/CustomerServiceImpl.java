a41917e3e087d99ba0ce8d21006a1935c52f008e
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2022-07-13 20:58:45 +0530

    customerServiceImpl saveCustomer content updated

--- src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
+++ src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
@@ -1,4 +1,59 @@
 package lk.ijse.spring.service.impl;
 
-public class CustomerServiceImpl {
+import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.entity.Customer;
+import lk.ijse.spring.repo.CustomerRepo;
+import lk.ijse.spring.service.CustomerService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+
+@Transactional
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    @Autowired
+    private CustomerRepo customerRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+
+    @Override
+    public void saveCustomer(CustomerDTO customerDTO) {
+        if (!customerRepo.existsById(customerDTO.getCus_ID())){
+            customerRepo.save(modelMapper.map(customerDTO, Customer.class));
+        }else {
+            throw new RuntimeException("Customer Already Exist");
+        }
+    }
+
+    @Override
+    public void deleteCustomer(String customerID) {
+
+    }
+
+    @Override
+    public void updateCustomer(CustomerDTO customerDTO) {
+
+    }
+
+    @Override
+    public List<CustomerDTO> searchCustomer(String id, String name) {
+        return null;
+    }
+
+    @Override
+    public List<CustomerDTO> getAllCustomers() {
+        return null;
+    }
+
+    @Override
+    public String generateCustomerId() {
+        return null;
+    }
 }
