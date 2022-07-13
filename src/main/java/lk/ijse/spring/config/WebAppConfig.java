69dcf0c1c8da5f742cb096e83a88bb1f91b35793
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2022-07-13 16:43:05 +0530

    aded config package and content

---
+++ src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -0,0 +1,25 @@
+package lk.ijse.spring.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * @author Helitha Sri
+ * @created 6/22/2022 - 1:27 PM
+ * @project Spring POS
+ */
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.spring")
+public class WebAppConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+
+}
