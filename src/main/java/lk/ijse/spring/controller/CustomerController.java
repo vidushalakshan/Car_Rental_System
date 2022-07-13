9dc47ff3663de5eab421274207fac707bf0d9d85
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2022-07-14 00:02:55 +0530

    updated customerController Content

--- src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,13 +1,12 @@
 package lk.ijse.spring.controller;
 
+import lk.ijse.spring.dto.CustomerDTO;
 import lk.ijse.spring.service.CustomerService;
 import lk.ijse.spring.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/customer")
@@ -21,5 +20,35 @@ public class CustomerController {
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseUtil getAllCustomer(){
         return  new ResponseUtil(200, "OK",customerService.getAllCustomers());
+    }
+
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil addCustomer(@ModelAttribute CustomerDTO customerDTO) {
+        customerService.saveCustomer(customerDTO);
+        return new ResponseUtil(200, "Customer added Successfully", null);
+    }
+
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil updateCustomer(@RequestBody CustomerDTO customerDTO) {
+        customerService.updateCustomer(customerDTO);
+        return new ResponseUtil(200, "Customer Update Successfully", null);
+    }
+
+    @DeleteMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil deleteCustomer(@RequestParam String id) {
+        customerService.deleteCustomer(id);
+        return new ResponseUtil(200, "Customer Delete Successfully", null);
+    }
+
+    @GetMapping(path = "/search", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil searchCustomer(@RequestParam String search) {
+        return new ResponseUtil(200, "OK", customerService.searchCustomer(search, search));
+    }
+
+    @GetMapping(path = "/generate", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil generateId() {
+        return new ResponseUtil(200, "OK", customerService.generateCustomerId());
     }
 }
