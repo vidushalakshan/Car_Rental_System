47351dbf0185fb983c8196b40c7ae5f616a74362
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2022-08-01 12:17:08 +0530

    aded frontEnd

--- src/main/java/lk/ijse/spring/repo/StafRepo.java
+++
@@ -1,7 +0,0 @@
-package lk.ijse.spring.repo;
-
-import lk.ijse.spring.entity.Staf;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StafRepo extends JpaRepository<Staf, String> {
-}
