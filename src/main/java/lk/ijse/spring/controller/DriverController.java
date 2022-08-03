3ce727147e76db4fd108b1676927b92651e9d424
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2022-08-03 13:47:53 +0530

    updated driverController content

--- src/main/java/lk/ijse/spring/controller/DriverController.java
+++ src/main/java/lk/ijse/spring/controller/DriverController.java
@@ -1,4 +1,50 @@
 package lk.ijse.spring.controller;
 
+import lk.ijse.spring.dto.DriversDTO;
+import lk.ijse.spring.service.DriverService;
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("api/driver")
+@CrossOrigin
 public class DriverController {
+
+    @Autowired
+    DriverService driverService;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil getAllCustomer(){
+        return  new ResponseUtil(200, "OK",driverService.getAllDrivers());
+    }
+
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil addDriver(@ModelAttribute DriversDTO driversDTO) {
+        driverService.saveDriver(driversDTO);
+        return new ResponseUtil(200, "Driver added Successfully", null);
+    }
+
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil updateDriver(@RequestBody DriversDTO driversDTO) {
+        driverService.updateDriver(driversDTO);
+        return new ResponseUtil(200, "Driver Update Successfully", null);
+    }
+
+    @DeleteMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil deleteDriver(@RequestParam String id) {
+        driverService.deleteDriver(id);
+        return new ResponseUtil(200, "Driver Delete Successfully", null);
+    }
+
+    @GetMapping(path = "/search", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil searchDriver(@RequestParam String search) {
+        return new ResponseUtil(200, "OK", driverService.searchDriver(search));
+    }
+
 }
